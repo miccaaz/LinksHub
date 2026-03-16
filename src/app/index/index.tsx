fbeb90d701133f4b73cfc86ee6919c63b3400a9f
Miguel <mclcastroloureiro@gmail.com>
2026-03-16 15:10:06 -0300

    Implementa a estrutura inicial do aplicativo com componentes de navegação, categorias e links

---
+++ src/app/index/index.tsx
@@ -0,0 +1,51 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { View, Image, TouchableOpacity, FlatList, Modal, Text } from "react-native";
+
+import { colors } from "@/styles/colors";
+import { styles } from "./styles";
+
+import { Link } from "@/components/link";
+import { Categories } from "@/components/categories";
+
+export default function Index() {
+  return (
+    <View style={ styles.container }>
+      <View style={ styles.header }>
+        <Image source={ require("@/assets/logo.png") } style={ styles.logo } />
+
+        <TouchableOpacity activeOpacity={0.5}>
+          <MaterialIcons name="add" size={32} color={colors.red[300]} />
+        </TouchableOpacity>
+      </View>
+
+      <Categories />  
+
+      <FlatList
+        data={ [1, 2, 3, 4, 5, 6, 7, 8] }
+        renderItem={ () => (
+          <Link name="Google" url="https://www.google.com" onDetails={() => {}} />
+        )}
+        style = {styles.links}
+        contentContainerStyle = {styles.linksContent}
+        showsVerticalScrollIndicator={false}
+      />
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Cursos</Text>
+              <TouchableOpacity activeOpacity={0.5}>
+                <MaterialIcons name="close" size={20} color={colors.gray[400]} />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>Google</Text>
+            <Text style={styles.modalUrl}>https://www.google.com</Text>
+          </View>
+        </View>
+      </Modal>
+      
+    </View>
+  ) 
+}
+
---
+++ src/components/category/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingLeft: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 5
+  },
+  name:{
+    fontSize: 16,
+    fontWeight: '600'
+  }
+})
